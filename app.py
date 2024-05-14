34f701205d61c41bc66ce285decd96be9584b19c
Joao Henrique <glasnote@gmail.com>
2024-05-14 11:51:24 -0300

    atualizacao controller

--- app.py
+++ app.py
@@ -4,15 +4,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def principal():
-    return render_template('app.html')
+    titulo = 'O segredo'
+    tituloPagina = 'Esse é o subtitulo da página'
+    return render_template('app.html', titulo = titulo, tituloPagina = tituloPagina)
 
 @app.route("/cadastro")
 def cadastro():
     return 'Olá sou uma rota de casdastro página de casdastro'
 
-@app.route("/login")
+@app.route("/login" , methods=['GET', 'POST'])
 def login():
     return 'realizar Login'
 
 if  __name__ == "__main__":
-    app.run(debug=True,Port=6000)
+    app.run(debug=True)
